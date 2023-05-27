f5ff6fd347e73945dd8b8f4a14e38cd7feeb17a0
chris93solomon11@gmail.com <chris93solomon11@gmail.com>
2023-05-27 17:01:24 -0400

    chapter 9 classes

---
+++ chapter_9/login_attempts.py
@@ -0,0 +1,41 @@
+#exercise 9-5: login attempts
+
+#copy of exercise 9-3: user class
+class User:
+    """a simple class to describe a user profile."""
+    
+    def __init__(self, first_name, last_name, department, age):
+        """Initialize the user profile."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.department = department
+        self.age = age
+        self.login_attempts = 0
+
+    
+    def describe_user(self):
+        """describe the user profile."""
+        print(f'the following is the user information: ')
+        print(f'\nFirst Name: {self.first_name} ')
+        print(f'\nLast Name: {self.last_name}')
+        print(f'\nDepartment: {self.department}')
+        print(f'\nAge: {self.age}')
+        
+    def greet_user(self):
+        """greet the new user"""
+        print(f'\nWelcome to the company ' \
+              f' {self.first_name.title()} {self.last_name.title()}') 
+        
+    def increment_login_attempts(self):
+        """add the number of login attempts by 1. 
+        simulate the user logging in. 
+        """
+        self.login_attempts += 1
+
+    def reset__login_attempts(self):
+        """reset the number of login attempts to 0"""
+        self.login_attempts = 0
+
+
+christopher = User('christopher', 'solomon', 'information technology', 30)
+print(christopher.describe_user())
