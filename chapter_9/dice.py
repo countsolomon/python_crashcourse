9032e90a1a18533b7f8e042ce43331fdaa83738b
Christopher Solomon <countsolomon@outlook.com>
2023-06-20 11:46:42 -0400

    dice throw

--- chapter_9/dice.py
+++ chapter_9/dice.py
@@ -7,15 +7,42 @@ from random import randint as RI
 class Die:
     """The class that portrays a 6 sided die."""
 
-    def __init__(self, sides):
+    def __init__(self, sides=6):
         """Initialize the die class."""
-        self.sides = 6
+        self.sides = sides
 
     
-    def roll_die(self, sides):
-        """simulate rolling the dice"""
-        rolling = RI(1, sides)  
-        print(rolling)
+    def roll_die(self):
+        """return a number between 1 and the sides of die"""
+        return RI(1, self.sides)
 
-default_size = Die(6)
-default_size.roll_die(6)
+
+#simulate rolling a 6-sided die
+d6 = Die()
+
+results = []
+for roll_num in range(10):
+    result = d6.roll_die()
+    results.append(result)
+print("10 rolls of a 6-sided die: ")
+print(results)
+
+#simulate rolling a 10-sided die
+d10 = Die(sides=10)
+
+results = []
+for roll_num in range(10):
+    result = d10.roll_die()
+    results.append(result)
+print("\n10 rolls of a 10-sided die: ")
+print(results)
+
+#simulate rolling a 20-sided die
+d20 = Die(sides=20)
+
+results = []
+for roll_num in range(10):
+    result = d20.roll_die()
+    results.append(result)
+print("\n10 rolls of a 20-sided die: ")
+print(results)
